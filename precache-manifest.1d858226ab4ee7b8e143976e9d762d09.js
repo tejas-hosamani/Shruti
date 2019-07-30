a2e4832a2eea9385d0b8ac6046fed7ca361e7619
Tejas H <tejas007tez@gmail.com>
2019-07-30 13:25:32 +0530

    Updates

---
+++ precache-manifest.1d858226ab4ee7b8e143976e9d762d09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09a78c0c1be7996a50ecb506e1901c3d",
+    "url": "/Shruti/index.html"
+  },
+  {
+    "revision": "ec97df1224c16a6af4e9",
+    "url": "/Shruti/static/css/main.a975fe5a.chunk.css"
+  },
+  {
+    "revision": "136b1a3b56916362feda",
+    "url": "/Shruti/static/js/2.9e0431f3.chunk.js"
+  },
+  {
+    "revision": "ec97df1224c16a6af4e9",
+    "url": "/Shruti/static/js/main.c280cb97.chunk.js"
+  },
+  {
+    "revision": "94da335eca7267d4b736",
+    "url": "/Shruti/static/js/runtime~main.3529e798.js"
+  },
+  {
+    "revision": "1f2c801c4e8edd0c9e90c95860c8a990",
+    "url": "/Shruti/static/media/cover.1f2c801c.jpg"
+  }
+]);
